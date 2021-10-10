aea00fe993f38cb22fd945b4965a413aca3fadf3
NurHidayatul190014 <85481986+NurHidayatul190014@users.noreply.github.com>
2021-10-10 21:16:36 +0700

    Update knapsack.py
    almost done

--- knapsack.py
+++ knapsack.py
@@ -1,23 +1,12 @@
-def textToBinary(kata_str):
-    print("String: " + str(kata_str))
+def textToBinary(kata_str): #convert text ke binary
     res = ''.join(format(ord(i), '08b') for i in kata_str)
-    print("String to Binary: " + str(res))
+    print("teks ke Binary: " + str(res))
     return str(res)
 
-def binaryToText(s):
-    print("Binary: "+s)
+def binaryToText(s): #convert binary ke text
     binary = ''.join(chr(int(s[i*8:i*8+8],2)) for i in range(len(s)//8))
-    print("Binary to String: "+ binary)
+    print("Binary ke teks: "+ binary)
     return binary
-
-#membuat kunci sementara untuk proses dekripsi
-def temp_key(key):
-    list_key = []
-
-    for i in key:
-        list_key.append(i)
-    
-    return list_key
 
 def generatePublicKey(superincrasing, m ,n):  #membuat public key
     publicKey = []
@@ -31,7 +20,7 @@ def enkripsi(plaintext, publicKey):
     teks = ""  #menampung teks sementara sebelum di masukkan list
     for i in plaintext: #membagi dan memasukkan text dalam bentuk blok ke var list_text
         teks += i
-        if len(teks) == 6:
+        if len(teks) == 4:
             list_text.append(teks)
             teks = ""
     
@@ -48,8 +37,6 @@ def enkripsi(plaintext, publicKey):
         indeks = 0
         kriptogram = 0
 
-    print("list chipper  ", list_chiperteks)
-
     return list_chiperteks
 
 def dekripsi(privateKey, m, n, list_chiperteks):
@@ -65,60 +52,53 @@ def dekripsi(privateKey, m, n, list_chiperteks):
     for i in list_chiperteks:
         hasil = i * n_min1 % m
         list_hasil.append(hasil)
-    
-    print("hasil mod ", list_hasil)
 
     # proses deksripsi ke binary teks
     #hasil mod di buat binary
     plaintext = ""
 
-    for i in list_hasil:
+    for i in list_hasil: #tiap hasil mod dicari biner nya
         hasil_mod = i
-        temp = temp_key(privateKey)
-        ulang = True
-
-        while ulang:
-            key = temp[0]
-            count = key
-
-            for i in range(len(temp) - 1, 0, -1):
-                count += temp[i]
-                
-                if count > hasil_mod:
-                    temp.remove(temp[i])
-                    break
-                elif len(temp) == 4:
-                    if key + temp[1] == hasil_mod:
-                        ulang = False
-                        break
-                    else:
-                        temp = temp_key(privateKey)
-                        temp.remove(key)
-                elif count == hasil_mod:
-                    ulang = False
-        print("temp  ", temp)
-        for i in privateKey:
-            if i in temp:
-                plaintext += "1"
+        temp_plaintext = []
+        for i in range(len(privateKey) - 1, -1, -1): #pengurangan hasil modulus dengan isi private key
+            if hasil_mod >= privateKey[i]:
+                hasil_mod -= privateKey[i]
+                temp_plaintext.append("1")
             else:
-                plaintext += "0"
+                temp_plaintext.append("0")
+
+        # yang ada di temp_plaintext itu masih terbalik biner nya
+        #jadi ini di urutkan terus di jadikan satu di var plaintext nya
+        for i in range(len(temp_plaintext) - 1, -1, -1):
+            plaintext += temp_plaintext[i]
+
+    return plaintext
 
 
 
-    print("plaintetxt = ", plaintext)
-
-
-
-privateKey= [2, 3, 6, 13, 27, 52]
+privateKey= [2, 3, 6, 13]
 m = 105
 n = 31
 publicKey = generatePublicKey(privateKey, m , n)
-print(publicKey)
 
-masuk = "hayaku"
+print("+++++++ algoritma knapsack (merkle-hellman)+++++")
+print("kunci private: ", privateKey)
+print("kunci publik: ",publicKey)
 
+# input teks
+masuk = input(str("masukkan kata yang diinginkan = "))
+print("")
 
+# enkripsi
+print("+++++++ enkripsi ++++++")
 plaintext = textToBinary(masuk)
 list_chiperteks =  enkripsi(plaintext, publicKey)
+print("hasil chiperteks: ", list_chiperteks)
 
-dekripsi(privateKey, m, n, list_chiperteks)
+#dekripsi
+print("")
+print("+++++++ dekripsi ++++++")
+deskrip = dekripsi(privateKey, m, n, list_chiperteks)
+
+
+binaryToText(deskrip)
